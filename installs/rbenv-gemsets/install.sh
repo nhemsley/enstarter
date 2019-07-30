c7eb8cdfede79942a616abc78c816a87b24b7523
Nicholas Hemsley <nick.hems@gmail.com>
2019-07-31 02:52:16 +1000

    fsaf

--- installs/rbenv-gemsets/install.sh
+++ installs/rbenv-gemsets/install.sh
@@ -1,2 +1,2 @@
-mkdir -p "$(rbenv root)"/plugins
-git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
+mkdir -p ~/.rbenv/plugins
+git clone git://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
--- installs/rbenv-ruby-build/install.sh
+++ installs/rbenv-ruby-build/install.sh
@@ -1,2 +1,2 @@
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
