9a5b4f73f3484e9263f99cd1c879af0fcb2eb44f
Nicholas Hemsley <nick.hems@gmail.com>
2019-07-31 02:49:50 +1000

    safd

--- commands/install.sh
+++ commands/install.sh
@@ -1,9 +1,8 @@
-TO_INSTALL=ls -d $ENSTARTER_INSTALLS_DIR/*/ |sort
 
+TO_INSTALL=$(ls -d $ENSTARTER_INSTALLS_DIR/*/)
 for install in $TO_INSTALL; do
   INSTALL_FILE=$install/install.sh
   [ -f $INSTALL_FILE ] && source $INSTALL_FILE
-
   ENV_FILE=$install/env.sh
   [ -f $ENV_FILE ] && source $ENV_FILE
 done
