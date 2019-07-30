e11da0e9a48b0fc44d158bd164cf3721202c55da
Nicholas Hemsley <nick.hems@gmail.com>
2019-07-31 02:16:08 +1000

    rbenv plugins

---
+++ installs/fbenv-gemsets/install.sh
@@ -0,0 +1,2 @@
+mkdir -p "$(rbenv root)"/plugins
+git clone git://github.com/jf/rbenv-gemset.git $HOME/.rbenv/plugins/rbenv-gemset
---
+++ installs/rbenv-ruby-build/install.sh
@@ -0,0 +1,2 @@
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
