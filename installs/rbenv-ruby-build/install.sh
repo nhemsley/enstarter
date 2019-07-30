72954bf89c7430925eadd9d6cf147887984a7714
Nicholas Hemsley <nick.hems@gmail.com>
2019-07-31 02:56:05 +1000

    typo

--- installs/rbenv-ruby-build/install.sh
+++ installs/rbenv-ruby-build/install.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/.rbenv/plugins
-[ ! -d ~/.rbenv/plugins/ruby-build ] git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+[ ! -d ~/.rbenv/plugins/ruby-build ] && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
